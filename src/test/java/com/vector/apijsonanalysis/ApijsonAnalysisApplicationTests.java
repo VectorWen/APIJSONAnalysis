2b207cf85952d9842c9e53545db6c1e39a4eadb4
vector.huang <vectorhuang@163.com>
2023-02-13 16:49:51 +0800

    导入APIJSONDemo-HikariCP

---
+++ src/test/java/com/vector/apijsonanalysis/ApijsonAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vector.apijsonanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApijsonAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
